91e5f864e3e8fc657c278f445de3d4dc751b0cf0
Ansh Raghuvanshi <121013240+2611ansh@users.noreply.github.com>
2023-10-02 18:47:33 +0000

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 
-from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+from sklearn.metrics import accuracy_score
 
 def save_object(file_path, obj):
     try:
@@ -27,15 +27,11 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
             model = list(models.values())[i]
             # Train model
             model.fit(X_train,y_train)
-
-            
-
             # Predict Testing data
             y_test_pred =model.predict(X_test)
 
-            # Get R2 scores for train and test data
-            #train_model_score = r2_score(ytrain,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
+            # Get accuarcy score
+            test_model_score = accuracy_score(y_test,y_test_pred)
 
             report[list(models.keys())[i]] =  test_model_score
 
