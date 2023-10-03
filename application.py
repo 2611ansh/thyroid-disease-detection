d309ed1f4d2029219b0d75c88583c7215f7dbfba
Ansh Raghuvanshi <121013240+2611ansh@users.noreply.github.com>
2023-10-03 08:10:51 +0000

    application file updated

--- application.py
+++ application.py
@@ -7,34 +7,34 @@ application = Flask(__name__)
 def home_page():
     return "Welcome to the Thyroid Detection App"
 
-@application.route('/predict', methods=['POST'])
+@application.route('/predict', methods=['GET', 'POST'])
 def predict_datapoint():
     data = request.form
 
     custom_data = CustomData(
-        age=float(request.form.get('age')),  # Include relevant form fields
-            sex=request.form.get('sex'),
-            on_thyroxine=request.form.get('on_thyroxine'),
-            query_on_thyroxine=request.form.get('query_on_thyroxine'),
-            on_antithyroid_medication=request.form.get('on_antithyroid_medication'),
-            sick=request.form.get('sick'),
-            pregnant=request.form.get('pregnant'),
-            thyroid_surgery=request.form.get('thyroid_surgery'),
-            i131_treatment=request.form.get('i131_treatment'),
-            query_hypothyroid=request.form.get('query_hypothyroid'),
-            query_hyperthyroid=request.form.get('query_hyperthyroid'),
-            lithium=request.form.get('lithium'),
-            goitre=request.form.get('goitre'),
-            tumor=request.form.get('tumor'),
-            hypopituitary=request.form.get('hypopituitary'),
-            psych=request.form.get('psych'),
-            TSH_measured=request.form.get('TSH_measured'),
-            T3_measured=request.form.get('T3_measured'),
-            TT4_measured=request.form.get('TT4_measured'),
-            T4U_measured=request.form.get('T4U_measured'),
-            FTI_measured=request.form.get('FTI_measured'),
-            TBG_measured=request.form.get('TBG_measured'),
-            referral_source=request.form.get('referral_source')
+        age=request.form.get('age'),  # Include relevant form fields
+        sex=request.form.get('sex'),
+        on_thyroxine=request.form.get('on_thyroxine'),
+        query_on_thyroxine=request.form.get('query_on_thyroxine'),
+        on_antithyroid_medication=request.form.get('on_antithyroid_medication'),
+        sick=request.form.get('sick'),
+        pregnant=request.form.get('pregnant'),
+        thyroid_surgery=request.form.get('thyroid_surgery'),
+        i131_treatment=request.form.get('i131_treatment'),
+        query_hypothyroid=request.form.get('query_hypothyroid'),
+        query_hyperthyroid=request.form.get('query_hyperthyroid'),
+        lithium=request.form.get('lithium'),
+        goitre=request.form.get('goitre'),
+        tumor=request.form.get('tumor'),
+        hypopituitary=request.form.get('hypopituitary'),
+        psych=request.form.get('psych'),
+        TSH_measured=request.form.get('TSH_measured'),
+        T3_measured=request.form.get('T3_measured'),
+        TT4_measured=request.form.get('TT4_measured'),
+        T4U_measured=request.form.get('T4U_measured'),
+        FTI_measured=request.form.get('FTI_measured'),
+        TBG_measured=request.form.get('TBG_measured'),
+        referral_source=request.form.get('referral_source')
     )
 
     final_new_data = custom_data.get_data_as_dataframe()
